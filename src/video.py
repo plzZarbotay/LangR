673bb07b2765a42faba913753835a108f98152b7
Art'S <94591965+plzZarbotay@users.noreply.github.com>
2023-03-31 20:53:37 +0300

    Update video.py

--- src/video.py
+++ src/video.py
@@ -4,38 +4,68 @@ import numpy as np
 from tabulate import tabulate
 from tqdm.autonotebook import tqdm
 
+
 class Video:
     def __init__(self, video_path):
         self.video_path = video_path
-        self.cap = cv2.VideoCapture(video_path)
-        self.fps = self.cap.get(cv2.CAP_PROP_FPS)
-        self.frame_size = (int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-        self.num_frames = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        self.current_frame = 0
-        
+        self.video_cap = cv2.VideoCapture(video_path)
+
+        self.num_frames = self._get_num_frames()
+        self.fps = self._get_fps()
+        self.height, self.width = self._get_frames_dimension()
+
+    def __str__(self):
+        video_details = [
+            ['Video path', self.video_path],
+            ['Number of frames', self.num_frames],
+            ['FPS', self.fps],
+            ['(height, width)', f'({self.height}, {self.width})']
+        ]
+        return tabulate(video_details)
+
     def __iter__(self):
-        self.current_frame = 0
-        while self.current_frame < self.num_frames:
-            ret, frame = self.cap.read()
-            if not ret:
+        for _ in range(self.num_frames):
+            success, frame = self.video_cap.read()
+            if success is False or frame is None:
                 break
-            self.current_frame += 1
             yield frame
-            
-    def get_frame(self, frame_num):
-        if frame_num < 0 or frame_num >= self.num_frames:
-            raise ValueError(f"Invalid frame number {frame_num}")
-        self.cap.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
-        ret, frame = self.cap.read()
-        if not ret:
-            raise ValueError(f"Error reading frame {frame_num}")
-        return frame
-    
-    def get_video_writer(self, output_path, codec='mp4v', fps=None, frame_size=None):
-        if fps is None:
-            fps = self.fps
-        if frame_size is None:
-            frame_size = self.frame_size
-        fourcc = cv2.VideoWriter_fourcc(*codec)
-        writer = cv2.VideoWriter(output_path, fourcc, fps, frame_size)
-        return writer
+
+        self.video_cap.release()
+
+    def _get_num_frames(self):
+        num_frames = int(self.video_cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        assert num_frames > 0, 'The video contains 0 frames.'
+        return num_frames
+
+    def _get_fps(self):
+        return int(self.video_cap.get(cv2.CAP_PROP_FPS))
+
+    def _get_frames_dimension(self):
+        _, frame = cv2.VideoCapture(self.video_path).read()
+        height, width, channels = frame.shape
+        return height, width
+
+    def _frames_progress_bar(self, description):
+        return tqdm(enumerate(self), desc=description, unit='frame', total=self.num_frames)
+
+    def export_frames(self, frames_path):
+        os.makedirs(frames_path, exist_ok=True)
+        file_name_zero_padding = len(str(self.num_frames))
+        self.video_cap = cv2.VideoCapture(self.video_path)
+
+        for count, frame in self._frames_progress_bar(description='Exporting frames'):
+            frame_number = str(count).zfill(file_name_zero_padding)
+            frame_path = f'{frames_path}/{frame_number}.jpg'
+            cv2.imwrite(frame_path, frame)
+
+    def get_video_writer(self, video_path):
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        dimension = (self.width, self.height)
+        video_writer = cv2.VideoWriter(video_path, fourcc, self.fps, dimension)
+        return video_writer
+
+    def get_frames_tensor(self):
+        frames = []
+        for frame in self:
+            frames.append(frame)
+        return np.array(frames)
