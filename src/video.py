144f7caf5e5f37d9287d1a5d72c0d9672d62ebea
Art'S <94591965+plzZarbotay@users.noreply.github.com>
2023-03-31 19:22:42 +0300

    Update video.py

--- src/video.py
+++ src/video.py
@@ -7,47 +7,35 @@ from tqdm.autonotebook import tqdm
 class Video:
     def __init__(self, video_path):
         self.video_path = video_path
-        if os.path.isfile(video_path) and video_path.endswith(('.jpg', '.jpeg', '.png', '.bmp')):
-            self.num_frames = 1
-            self.fps = 1
-            self.height, self.width, _ = cv2.imread(video_path).shape
-        else:
-            self.video_cap = cv2.VideoCapture(video_path)
-            self.num_frames = self._get_num_frames()
-            self.fps = self._get_fps()
-            self.height, self.width = self._get_frames_dimension()
-
-    def __str__(self):
-        video_details = [
-            ['Video path', self.video_path],
-            ['Number of frames', self.num_frames],
-            ['FPS', self.fps],
-            ['(height, width)', f'({self.height}, {self.width})']
-        ]
-        return tabulate(video_details)
-
-    def _get_num_frames(self):
-        num_frames = int(self.video_cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        assert num_frames > 0, 'The video contains 0 frames.'
-        return num_frames
-
-    def _get_fps(self):
-        return int(self.video_cap.get(cv2.CAP_PROP_FPS))
-
-    def _get_frames_dimension(self):
-        _, frame = cv2.VideoCapture(self.video_path).read()
-        height, width, channels = frame.shape
-        return height, width
-
-    def _get_image_details(self):
-        height, width, channels = cv2.imread(self.video_path).shape
-        return height, width, channels
-
-    def get_frames_tensor(self):
-        if self.num_frames == 1:
-            return np.array([cv2.imread(self.video_path)], dtype=np.uint8)
-        else:
-            frames = []
-            for frame in self:
-                frames.append(frame)
-            return np.array(frames, dtype=np.uint8)
+        self.cap = cv2.VideoCapture(video_path)
+        self.fps = self.cap.get(cv2.CAP_PROP_FPS)
+        self.frame_size = (int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+        self.num_frames = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.current_frame = 0
+        
+    def __iter__(self):
+        self.current_frame = 0
+        while self.current_frame < self.num_frames:
+            ret, frame = self.cap.read()
+            if not ret:
+                break
+            self.current_frame += 1
+            yield frame
+            
+    def get_frame(self, frame_num):
+        if frame_num < 0 or frame_num >= self.num_frames:
+            raise ValueError(f"Invalid frame number {frame_num}")
+        self.cap.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
+        ret, frame = self.cap.read()
+        if not ret:
+            raise ValueError(f"Error reading frame {frame_num}")
+        return frame
+    
+    def get_video_writer(self, output_path, codec='mp4v', fps=None, frame_size=None):
+        if fps is None:
+            fps = self.fps
+        if frame_size is None:
+            frame_size = self.frame_size
+        fourcc = cv2.VideoWriter_fourcc(*codec)
+        writer = cv2.VideoWriter(output_path, fourcc, fps, frame_size)
+        return writer
